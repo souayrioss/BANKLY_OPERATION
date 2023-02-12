0065de2717403bdc3553102cf5e9b722d250532b
Oussama Souayri <souayri.oussama@gmail.com>
2023-02-12 10:05:05 +0100

    init commit

---
+++ src/test/java/org/roronoa/banklyoperation/BanklyOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.roronoa.banklyoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklyOperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
